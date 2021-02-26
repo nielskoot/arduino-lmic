6e78a02b6fa7b268c91239e79c8a0984c4869f6a
Niels Koot <niels.koot@mutelcor.com>
2021-02-26 15:49:17 +0100

    LMICbandplan_getJoinCFlistFirstChannel redef warn

--- src/lmic/lmic_bandplan_as923.h
+++ src/lmic/lmic_bandplan_as923.h
@@ -109,6 +109,7 @@ ostime_t LMICas923_nextJoinTime(ostime_t now);
 #define LMICbandplan_nextJoinTime(now)     LMICas923_nextJoinTime(now)
 
 // The CFList in join-accept first channel is 2 (unlike most EU like regions where it is 3)
+#undef LMICbandplan_getJoinCFlistFirstChannel
 #define LMICbandplan_getJoinCFlistFirstChannel()    (2)
 
 #endif // _lmic_bandplan_as923_h_
